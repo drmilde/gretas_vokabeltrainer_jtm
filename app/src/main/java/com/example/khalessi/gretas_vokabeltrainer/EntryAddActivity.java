7d0bde76868d88aeee13f9369ad7b6c34ca4da0b
drmilde <milde@hs-fulda.de>
2018-01-08 12:48:04 +0100

    Aktuelle UnitTitle wird in EntryAddActivity übernommen

--- app/src/main/java/com/example/khalessi/gretas_vokabeltrainer/EntryAddActivity.java
+++ app/src/main/java/com/example/khalessi/gretas_vokabeltrainer/EntryAddActivity.java
@@ -1,6 +1,5 @@
 package com.example.khalessi.gretas_vokabeltrainer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.khalessi.gretas_vokabeltrainer.database.Unit;
+import com.example.khalessi.gretas_vokabeltrainer.state.AppState;
 
 public class EntryAddActivity extends AppCompatActivity {
 
@@ -18,20 +21,28 @@ public class EntryAddActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        // this
+        showCurrentUnitTitle();
+
+        // processing clicks
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add a new vocab to the table", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                // TODO reset EditText fields to standard values
             }
         });
 
+        //
         Button btn_addEntry = (Button) findViewById(R.id.btn_addEntrySubmit);
         btn_addEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Eintrag generieren und in eine Datenbank eintragen
+                // TODO Eintrag in Datenbank einfügen, bzw. aktualisieren
             }
         });
 
@@ -42,6 +53,17 @@ public class EntryAddActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void showCurrentUnitTitle() {
+        Unit currentUnit = AppState.getInstance().getCurrentUnit();
+        if (currentUnit != null) {
+
+            // get the title of the current unit and set it in the EntryAddActivity
+            EditText et_entry_add_lektionstitel = (EditText)findViewById(R.id.et_entry_add_lektionstitel);
+            et_entry_add_lektionstitel.setText(AppState.getInstance().getCurrentUnit().getTitle());
+
+        }
     }
 
 }
