ec07ad3194355f1e7d4390c8f425b679efd0c0a2
drmilde <milde@hs-fulda.de>
2018-01-15 01:36:59 +0100

    Vokabeln eingefügt

--- app/src/main/java/com/example/khalessi/gretas_vokabeltrainer/helper/BasicVocabularyLoader.java
+++ app/src/main/java/com/example/khalessi/gretas_vokabeltrainer/helper/BasicVocabularyLoader.java
@@ -13,10 +13,50 @@ public class BasicVocabularyLoader {
     public BasicVocabularyLoader() {
 
         dbh = AppState.getInstance().getDatabaseHelper();
-
         dbh.recreateDatabase();
-        dbh.insertSomeVocs();
-        dbh.insertSomeUnits();
 
+        insertSomeVocs();
+        insertSomeUnits();
+
+    }
+
+    private void insertSomeVocs() {
+        dbh.insertVocabulary("livingRoom", "to learn", "lernen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to run", "laufen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to hide", "verstecken", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to seek", "suchen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to watch", "schauen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to ring", "klingeln", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to rise", "aufsteigen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to fall", "fallen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to sing", "singen", "verb, inifinitiv");
+        dbh.insertVocabulary("livingRoom", "to think", "denken", "verb, inifinitiv");
+
+        dbh.insertVocabulary("livingRoom", "a pillow", "ein Kissen", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "a potted plant", "eine Topfpflanze", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "a set of speakers", "ein Lautsprecherpaar", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "a stereo system", "eine Stereoanlage", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "a vase of flowers", "ein Vase mit Blumen", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the armchair", "der Sessel", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the bookcase", "das Bücherregal", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the carpet", "der Teppich", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the chair", "der Stuhl", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the chimney", "der Schornstein", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the cupboard", "die Vitrine", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the curtains", "die Gardinen", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the drapes", "die Gardinen, die Vorhänge", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the fireplace", "der Kamin", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the shades", "die Jalousien", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the sofa", "das Sofa", "beschreibung");
+        dbh.insertVocabulary("livingRoom", "the television set", "der Fernseher", "beschreibung");
+    }
+
+    private void insertSomeUnits() {
+        dbh.insertUnit("livingRoom", "Greta", "In the living room", "On top of the world.");
+        dbh.insertUnit("GoingToSchool", "Greta", "Going to school", "This is less :)");
+        dbh.insertUnit("HavingAParty", "Greta", "Having a party", "Not yet, my dear.");
+        dbh.insertUnit("MeltingChocolate", "Greta", "Melting chocolate", "Smells good.");
+        dbh.insertUnit("MakingATorch", "Greta", "Making a torch", "You light up my life.");
+        dbh.insertUnit("KitchenCleaning", "Greta", "Kitchen cleaning", "Help me, supermom.");
     }
 }
